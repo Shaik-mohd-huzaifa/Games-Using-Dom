cef5262a1957953c932f124e463c66414d46a798
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2022-11-03 23:11:34 +0530

    Stored all the html elements in variables

---
+++ Game 2/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let model = document.querySelectorAll(".modal");
+let modelContent = document.querySelectorAll(".Model-content");
+let modelContentrem;
+let overlay = document.querySelector(".overlay");
+let closewindow = document.querySelector(".closewindow");
+console.log(model.length);
