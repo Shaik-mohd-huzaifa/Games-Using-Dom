78d3cd40ef68c42e3fed589eeb7507e63f68a884
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2022-11-04 22:50:15 +0530

    Add loop with event listener to add the active class to the modelcontent and overlay

--- Game 2/main.js
+++ Game 2/main.js
@@ -5,3 +5,12 @@ let modelContentrem;
 let overlay = document.querySelector(".overlay");
 let closewindow = document.querySelector(".closewindow");
 console.log(model.length);
+
+// Adds the eventlistener to add active
+for (let i = 0; i < model.length; i++) {
+  model[i].addEventListener("click", function () {
+    // console.log(model[i].id);
+    modelContent[i].classList.add("active");
+    overlay.classList.add("active");
+  });
+}
