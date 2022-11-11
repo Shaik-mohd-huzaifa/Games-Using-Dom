1591b926a4ae0185672dd7e964cd02229ddf531d
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2022-11-11 22:54:56 +0530

    add the popup elements to the variable to easy access

---
+++ GAME 3/main.js
@@ -0,0 +1,19 @@
+"use strict";
+// Storing the elements into variables
+
+// PopUp container Elements
+const closeButton = document.querySelector(".close");
+const playerDetailsEle = document.querySelector(".playerDetails");
+const playerNameInput = document.querySelector(".playernameinput");
+const nextbutton = document.querySelector(".next-ok");
+let okbutton;
+const previousbutton = document.querySelector(".previous");
+let player1Name;
+let player2Name;
+// const errormessage = document.querySelector(".error");
+const playerHeader = document.querySelector(".playerNumber");
+
+// Game elements
+
+const player1NameDisplay = document.getElementById("name--0");
+const player2NameDisplay = document.getElementById("name--1");
